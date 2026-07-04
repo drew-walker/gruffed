a584dae0ea57600ffa5b0e7c963c5bb0339b48d5
Drew Walker <drew.walker@gmail.com>
2026-07-04 12:37:45 -0700

    Fix site GitHub link

--- packages/site/lib/links.ts
+++ packages/site/lib/links.ts
@@ -1,2 +1,2 @@
 export const docsUrl = process.env.NEXT_PUBLIC_GRUFFED_DOCS_URL ?? "https://gruffed-docs.vercel.app/docs/cli";
-export const githubUrl = "https://github.com/drewwalker/gruffed";
+export const githubUrl = "https://github.com/drew-walker/gruffed";
